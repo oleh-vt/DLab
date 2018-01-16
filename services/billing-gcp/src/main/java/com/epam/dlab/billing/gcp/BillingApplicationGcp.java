fce31c485496349e06138a9028a04c995a691e91
bohdan.hliva <bohdan.hliva@datalex.com>
2018-01-16 16:49:26 +0200

    EPMCDLAB-845 billing functionality for gcp skeleton added

--- services/billing-gcp/src/main/java/com/epam/dlab/billing/gcp/BillingApplicationGcp.java
+++ services/billing-gcp/src/main/java/com/epam/dlab/billing/gcp/BillingApplicationGcp.java
@@ -16,12 +16,48 @@
 
 package com.epam.dlab.billing.gcp;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.cloudbilling.Cloudbilling;
+import com.google.api.services.cloudbilling.model.BillingAccount;
+import com.google.api.services.cloudbilling.model.ListBillingAccountsResponse;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Collections;
 
 @Slf4j
 public class BillingApplicationGcp {
 
-    public static void main(String[] args) {
-        log.info("===========BILLING GCP APPLICATION=========");
+    public static void main(String[] args) throws Exception {
+        System.out.println("=========Billing application========");
+        GoogleCredential credential = GoogleCredential.getApplicationDefault();
+        if (credential.createScopedRequired()) {
+            credential = credential.createScoped(Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
+        }
+
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        Cloudbilling cloudbillingService = new Cloudbilling.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("Google Cloud Platform Sample")
+                .build();
+
+        Cloudbilling.BillingAccounts.List request = cloudbillingService.billingAccounts().list();
+        ListBillingAccountsResponse response = request.execute();
+
+        do {
+            response = request.execute();
+            if (response.getBillingAccounts() == null)
+                continue;
+
+            for (BillingAccount billingAccount : response.getBillingAccounts()) {
+                System.out.println(billingAccount.getName());
+            }
+
+            request.setPageToken(response.getNextPageToken());
+        } while (response.getNextPageToken() != null);
     }
 }
