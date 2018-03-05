5e3da8884ac696c487a8333f581d7f9d800c5244
ochaparin <Oleksandr_Chaparin@epam.com>
2018-03-05 13:22:33 +0200

    Debugging in progress

--- integration-tests/src/test/java/com/epam/dlab/automation/test/TestServices.java
+++ integration-tests/src/test/java/com/epam/dlab/automation/test/TestServices.java
@@ -182,14 +182,16 @@ public class TestServices {
 			LOGGER.info("Client Id: {}", azureAuthData.getClientId());
 			LOGGER.info("Client secret: {}", azureAuthData.getClientSecret());
 
-			String appId = "6c3410d9-9867-4e35-abed-0eab8dd00e61";
-//			AzureADToken token = AzureADAuthenticator
-//					.getTokenUsingClientCreds(azureAuthData.getActiveDirectoryEndpointUrl(),
-//							appId, azureAuthData.getClientSecret());
-			String username = ConfigPropertyValue.getUsername();
-			String pass = ConfigPropertyValue.getPassword();
-			AzureADToken token = AzureADAuthenticator.getTokenUsingUserCreds(appId, username,
-					pass);
+			String authEndpoint = "https://login.microsoftonline.com/%s/oauth2/token";
+
+//			String appId = "6c3410d9-9867-4e35-abed-0eab8dd00e61";
+			AzureADToken token = AzureADAuthenticator
+					.getTokenUsingClientCreds(String.format(authEndpoint, azureAuthData.getTenantId()),
+							azureAuthData.getClientId(), azureAuthData.getClientSecret());
+//			String username = ConfigPropertyValue.getUsername();
+//			String pass = ConfigPropertyValue.getPassword();
+//			AzureADToken token = AzureADAuthenticator.getTokenUsingUserCreds(appId, username,
+//					pass);
 			LOGGER.info("Obtained token {} with date of expire {}", token.accessToken, token.expiry);
 			NamingHelper.setSsnToken(token.accessToken);
 			LOGGER.info("3a. Check login");
